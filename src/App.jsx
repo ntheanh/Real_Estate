409cf59e0a710d7ee90c013fb152eb6273c8f718
TheAnh <tozaco1@gmail.com>
2024-04-08 22:13:24 +0700

    navbar_responsive

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Navbar from "./components/navbar/Navbar"
+import "./Layout.scss"
+
+function App() {
+  return (
+    <div className="layout">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
